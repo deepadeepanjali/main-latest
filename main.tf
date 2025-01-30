59f012eef99c3c5676c78ddf45b1779345c4466c
deepadeepanjali <87965644+deepadeepanjali@users.noreply.github.com>
2025-01-30 12:47:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "keyvault-secret-rbac" {
   for_each            = var.bot_configurations
-  source = "git::https://github.com/deepadeepanjali/secret-test.git?ref=main"  
+  source = "git::https://github.com/deepadeepanjali/secret-latest.git?ref=main"  
   
   keyvault       = var.keyvault
   resourcegroup  = var.resourcegroup
